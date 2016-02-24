b6fb5ff5b8e3c7407f778664ee0a58aa7e63bc21
Chad Ostrowski <hi@chadoh.com>
2016-02-24 08:17:26 -0500

    Insertion Sort part 2

--- insertion_sort/2.rb
+++ insertion_sort/2.rb
@@ -1,40 +1,86 @@
-def insertionSort(ar)
-  e = ar[ar.length - 1]
-  (0...ar.length).each do |i|
-    if i == ar.length - 1
-      ar[0] = e
+def insertion_sort_single(ar)
+  e = ar[-1]
+  new_array = ar.dup
+  last_index = new_array.length - 1
+
+  (0...new_array.length).each do |i|
+    if i == last_index
+      new_array[0] = e
     else
-      ar[ar.length - 1 - i] = ar[ar.length - 2 - i]
-      if ar[ar.length - 1 - i] < e
-        ar[ar.length - 1 - i] = e
-        puts ar.join(' ')
+      new_array[last_index - i] = new_array[last_index - 1 - i]
+      if new_array[last_index - i] < e
+        new_array[last_index - i] = e
         break
       end
     end
-    puts ar.join(' ')
+  end
+
+  new_array
+end
+
+def insertionSort(ar, output = method(:puts))
+  output.call(ar.join(' ')) and return if ar.length < 2
+  (1...ar.length).each do |sub|
+    ar[0..sub] = insertion_sort_single(ar[0..sub])
+    output.call ar.join(' ')
   end
 end
 
-count = gets.to_i
-ar = gets.strip.split.map(&:to_i)
-# count = 10
-# ar = [2, 3, 4, 5, 6, 7, 8, 9, 10, 1]
-# ar = [2, 4, 6, 8, 3]
+# count = gets.to_i
+# ar = gets.strip.split.map(&:to_i)
 
-insertionSort(ar)
+# insertionSort(ar)
 
-# input:
-# 10
-# 2 3 4 5 6 7 8 9 10 1
-#
-# output:
-# 2 3 4 5 6 7 8 9 10 10
-# 2 3 4 5 6 7 8 9 9 10
-# 2 3 4 5 6 7 8 8 9 10
-# 2 3 4 5 6 7 7 8 9 10
-# 2 3 4 5 6 6 7 8 9 10
-# 2 3 4 5 5 6 7 8 9 10
-# 2 3 4 4 5 6 7 8 9 10
-# 2 3 3 4 5 6 7 8 9 10
-# 2 2 3 4 5 6 7 8 9 10
-# 1 2 3 4 5 6 7 8 9 10
+require "minitest/autorun"
+
+class Test < Minitest::Test
+  def test_default
+    ar = %w(1 4 3 5 6 2).map(&:to_i)
+    result = []
+
+    insertionSort(ar, result.method(:<<))
+
+    assert_equal(
+      result,
+      [
+        "1 4 3 5 6 2",
+        "1 3 4 5 6 2",
+        "1 3 4 5 6 2",
+        "1 3 4 5 6 2",
+        "1 2 3 4 5 6",
+      ]
+    )
+  end
+
+  def test_tricky
+    ar = %w(6 5 4 3 2 1).map(&:to_i)
+    result = []
+
+    insertionSort(ar, result.method(:<<))
+
+    assert_equal(
+      result,
+      [
+        "5 6 4 3 2 1",
+        "4 5 6 3 2 1",
+        "3 4 5 6 2 1",
+        "2 3 4 5 6 1",
+        "1 2 3 4 5 6",
+      ]
+    )
+  end
+
+  def test_short
+    ar = %w(1).map(&:to_i)
+    result = []
+
+    insertionSort(ar, result.method(:<<))
+
+    assert_equal(
+      result,
+      [
+        "1",
+      ]
+    )
+  end
+end
